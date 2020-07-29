ffda9ff0801e1f33b03f1c8125eda12c3d659961
j9che <j9che@student.cs.uwaterloo.ca>
2020-07-29 19:26:34 -0400

    fixed text segment error

--- os161-1.99/kern/arch/mips/locore/trap.c
+++ os161-1.99/kern/arch/mips/locore/trap.c
@@ -117,14 +117,19 @@ kill_curthread(vaddr_t epc, unsigned code, vaddr_t vaddr)
 
 	#if OPT_A3
 	struct addrspace *as;
+	struct proc *p = curproc;
 
 	as_deactivate();
 	as = curproc_setas(NULL);
 	as_destroy(as);
 
 	proc_remthread(curthread);
-	proc_destroy(curproc);
+	proc_destroy(p);
 	thread_exit();
+
+	kprintf("Fatal user mode trap %u sig %d (%s, epc 0x%x, vaddr 0x%x)\n",
+                code, sig, trapcodenames[code], epc, vaddr);
+        panic("I don't know how to handle this\n");
 
 	#else
 	kprintf("Fatal user mode trap %u sig %d (%s, epc 0x%x, vaddr 0x%x)\n",
